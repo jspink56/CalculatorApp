51d3cf504dbea90b8397a19756ecbcc2f0d7721a
jspink56 <jspink56@users.noreply.github.com>
2016-11-18 13:51:05 +0000

    Updated a comment in MainActivity.java

--- app/src/main/java/com/timbuchalka/calculator/MainActivity.java
+++ app/src/main/java/com/timbuchalka/calculator/MainActivity.java
@@ -172,4 +172,6 @@ public class MainActivity extends AppCompatActivity {
 	public String returnName() {
 		return "John was here";
 	}
+	
+	// This is a comment to test the Git pull process from Android Studio
 }
